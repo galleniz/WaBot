29ff89f5840349dbd9d4ff375ce8d5509219f314
xXMomazosNiz_69Xx <jalipa7u7@gmail.com>
2024-07-11 14:06:30 -0600

    refactor: new "Command" code, with js doc

--- src/Command.js
+++ src/Command.js
@@ -1,33 +1,41 @@
 const prefix = "!";
 
 class Command {
-    constructor(options){
+    constructor(options) {
         this.options = options;
         this.beta = options.beta || false;
         this.react = options.react || "nothing";
         this.name = options.name;
+        this.reactable = this.react !== "nothing" || options.reactable;
     }
-    async tryRun(message, client){
-        console.log(message.body);
- 
 
-        const [cmd, ...args] = message.body.slice(prefix.length).split(/ +/).map(str => str);
+    async tryRun(message, client) {
+        console.log(message.body);
+
+        const [cmd, ...args] = message.body.slice(prefix.length).split(/ +/);
         args.body = message.body;
-        let author = {
+        const author = {
             from: message.from,
             name: message._data.notifyName
+        };
+
+        const chat = await message.getChat();
+
+        if (this.reactable) {
+            message.react(this.react);
         }
-      
-        let chat = await message.getChat();
 
-        if (this.react !== "nothing")
-            message.react(this.react)
-
-
-        this.run(args, author, chat, message, client);
+        try {
+            this.run(args, author, chat, message, client);
+        } catch (err) {
+            console.error(`Error while executing "${this.name}"`);
+            console.log(err);
+        }
     }
-    async run(args, author, chat, message, client){
-        throw "No run field command";
+
+    async run(args, author, chat, message, client) {
+        throw new Error("No run field command");
     }
 }
+
 module.exports = Command;
