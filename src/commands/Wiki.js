8760c8bf38406a3e2ce75c0c75eb38de73e4e3fa
xXMomazosNiz_69Xx <jalipa7u7@gmail.com>
2024-07-11 14:08:43 -0600

    refactor: unnecessary code and minor fixes

--- src/commands/Wiki.js
+++ src/commands/Wiki.js
@@ -1,48 +1,55 @@
-
-const Command = require("../Command")
+const Command = require("../Command");
 const { MessageMedia } = require('whatsapp-web.js');
-const cache = new Map();
-class Wiki extends Command
-{
-    constructor()
-    {
+const wtf = require('wtf_wikipedia');
+
+/**
+ * Represents a command to search and retrieve information from Wikipedia.
+ * @extends Command
+ */
+class Wiki extends Command {
+    constructor() {
         super({
             name: "wiki",
             react: "🔍",
-
-        })
+        });
     }
-    run(args, author, chat, msg){
-        
-        const wtf = require('wtf_wikipedia');
 
-        async function searchWikipedia(query) {
-            try {
-            return await wtf.fetch(query);
-            } catch (err) {
-                chat.sendMessage("Has ocurred an error.")
-                console.error(err);
-            }
-        }
-
-        searchWikipedia(args.join(" ").toLowerCase())
-        .then(async (summary) =>{ 
+    /**
+     * Executes the wiki command.
+     * @param {string[]} args - The arguments passed to the command.
+     * @param {string} author - The author of the message.
+     * @param {Chat} chat - The chat where the command was executed.
+     * @param {Object} msg - The message object.
+     */
+    async run(args, author, chat, msg) {
+        try {
+            const summary = await searchWikipedia(args.join(" ").toLowerCase());
             let desc = summary.text();
-            if (desc.length > 4096) desc = desc.slice(0,4093) + "...";
+            if (desc.length > 4096) desc = desc.slice(0, 4093) + "...";
 
             const media = await MessageMedia.fromUrl(summary.images()[0].json().url);
             chat.sendMessage(desc);
-
             chat.sendMessage(media);
-        }) 
-        .catch(err => {
-            console.error(err)
-            chat.sendMessage("Has ocurred an error.")
-        });
-
-       
-
-      
+        } catch (err) {
+            console.error(err);
+            chat.sendMessage("Fallo al tratar de enviar el mensaje.");
+        }
     }
 }
+
+/**
+ * Searches Wikipedia for the given query.
+ * @param {string} query - The search query.
+ * @returns {Promise<Object>} - A promise that resolves to the fetched Wikipedia data.
+ * @throws {Error} - If there is an error while searching Wikipedia.
+ */
+async function searchWikipedia(query) {
+    try {
+        return await wtf.fetch(query);
+    } catch (err) {
+        console.error(err);
+        throw new Error("Fallo al buscar en Wikipedia, generalmente es porque el artículo no fue encontrado.");
+    }
+}
+
 module.exports = new Wiki();
