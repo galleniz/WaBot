dc87ec50dabdc7141022aa43c070bf9dd54e2660
xXMomazosNiz_69Xx <jalipa7u7@gmail.com>
2024-07-11 14:08:13 -0600

    refactor: docs

--- src/commands/SayCommand.js
+++ src/commands/SayCommand.js
@@ -1,17 +1,27 @@
 const Command = require("../Command")
 
-class SayCommand extends Command
-{
-    constructor()
-    {
+/**
+ * Represents a command that allows the bot to say a message in the chat.
+ * @extends Command
+ */
+class SayCommand extends Command {
+    constructor() {
         super({
             name: "say",
             react: "🧷",
-
-        })
+        });
     }
-    run(args, author, chat, msg){
-        chat.sendMessage(args.join(" "))
+
+    /**
+     * Executes the "say" command.
+     * @param {string[]} args - The arguments passed to the command.
+     * @param {string} author - The author of the command.
+     * @param {Chat} chat - The chat where the command was executed.
+     * @param {Message} msg - The message object associated with the command.
+     */
+    run(args, author, chat, msg) {
+        chat.sendMessage(args.join(" "));
     }
 }
+
 module.exports = new SayCommand();
