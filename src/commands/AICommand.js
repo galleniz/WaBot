66a6947d4df17ca9119e87a984e607c9aa61effd
xXMomazosNiz_69Xx <jalipa7u7@gmail.com>
2024-07-11 14:07:17 -0600

    refactor: Changed error message, and added docs

--- src/commands/AICommand.js
+++ src/commands/AICommand.js
@@ -3,30 +3,38 @@ const cleverbot = require("cleverbot-free")
 var map = new Map()
 
 
-class AICommand extends Command
-{
-    constructor()
-    {
+/**
+ * Represents a command for interacting with a chatbot.
+ * @extends Command
+ */
+class AICommand extends Command {
+    constructor() {
         super({
             name: "chatbot",
             react: "🤖",
-
-        })
+        });
     }
-    async run(args, author, chat, msg){
-        var a = map.get(author.from) || []
-        a.push(args.join(" "))
+
+    /**
+     * Executes the chatbot command.
+     * @param {Array} args - The arguments passed to the command.
+     * @param {Author} author - The author of the command.
+     * @param {Chat} chat - The chat where the command was executed.
+     * @param {Object} msg - The message object representing the command.
+     */
+    async run(args, author, chat, msg) {
+        var a = map.get(author.from) || [];
+        a.push(args.join(" "));
         try {
-            await cleverbot(args.join(" "),a).then(re=>{
+            await cleverbot(args.join(" "), a).then(re => {
                 a.push(re);
                 msg.reply(re);
-            })
+            });
         } catch {
-            msg.reply("Ha ocurrido un error en: AICommand.js:20 (Error: too much MS), trata de nuevo más tarde.")
+            msg.reply("Cleverbot se tardó mucho en responder. (error: 40xWAI timeout)");
         }
-        map.set(author.from,a);
-    
-
+        map.set(author.from, a);
     }
 }
+
 module.exports = new AICommand();
